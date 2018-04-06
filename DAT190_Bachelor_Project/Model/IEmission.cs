2028997a86e4bb746fc25b4ed1c6adf43718ea68
h181204 <fprytz@gmail.com>
2018-04-06 14:14:13 +0200

    Emission Interface created

---
+++ DAT190_Bachelor_Project/Model/IEmission.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace DAT190_Bachelor_Project.Model
+{
+    public interface IEmission
+    {
+        // Properties
+        int Id {get; set;}
+        double KgCO2 { get; set; }
+        DateTime Date { get; set; }
+
+        // Methods
+        double CalculateCO2();
+
+    }
+}
