66f6da909b64ffc675f5fe58f22d7feaa801da3e
h181204 <fprytz@gmail.com>
2018-04-06 14:05:01 +0200

    Initial check-in of module DAT190_Bachelor_Project

---
+++ DAT190_Bachelor_Project/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace DAT190_Bachelor_Project
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new DAT190_Bachelor_ProjectPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ DAT190_Bachelor_Project/DAT190_Bachelor_ProjectPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace DAT190_Bachelor_Project
+{
+    public partial class DAT190_Bachelor_ProjectPage : ContentPage
+    {
+        public DAT190_Bachelor_ProjectPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
