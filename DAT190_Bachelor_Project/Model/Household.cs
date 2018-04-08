3de267554c6ad3c8171b0627d3c0f93bce258fcc
h129411 <jorgenschreiner@hotmail.com>
2018-04-09 00:49:15 +0200

    Created Household Model

--- DAT190_Bachelor_Project/Model/Household.cs
+++ DAT190_Bachelor_Project/Model/Household.cs
@@ -1,19 +1,10 @@
 ﻿using System;
 namespace DAT190_Bachelor_Project.Model
 {
-    public class Household : IEmission
+    public class Household
     {
         public Household()
         {
-        }
-
-        public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public double KgCO2 { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public DateTime Date { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public double CalculateCO2()
-        {
-            throw new NotImplementedException();
         }
     }
 }
