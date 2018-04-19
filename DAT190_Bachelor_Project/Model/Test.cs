c8aaddf8312e8f667acbb3b86f70756d4736fa7e
h129411 <jorgenschreiner@hotmail.com>
2018-04-19 09:08:27 +0200

    Added Test class

---
+++ DAT190_Bachelor_Project/Model/Test.cs
@@ -0,0 +1,17 @@
+﻿using SkiaSharp;
+namespace DAT190_Bachelor_Project.Model
+{
+    public class Test
+    {
+
+        public Test(int value, SKColor color)
+        {
+            Value = value;
+            Color = color;
+        }
+
+        public int Value { private set; get; }
+
+        public SKColor Color { private set; get; }
+    }
+}
