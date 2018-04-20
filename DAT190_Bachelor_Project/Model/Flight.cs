0c76ee9b5768021900c86a4f641166e32799fce1
h181204 <fprytz@gmail.com>
2018-04-20 10:15:24 +0200

    Flight and vehicle class updated

--- DAT190_Bachelor_Project/Model/Flight.cs
+++ DAT190_Bachelor_Project/Model/Flight.cs
@@ -4,13 +4,7 @@ namespace DAT190_Bachelor_Project.Model
 {
     public class Flight : IEmission
     {
-        /* Constants used in formula explained, for more information see              * myclimate.org              * Average seat number (S), average seat number across al cabin classes.               * Passenger Load Factor (PLF) is the average load factor on flights              * Detour Constant (DC) is async distance correction for detours and               * holding patterns, and inefficiencies in the air traffic control systems.               * Cargo Factor (CF), to allocate some of the emissions to cargo load such              * as freight and mail. Number used is in reality (1 - CF).               * Weight Class (WC), takes into account the average seat area in the cabin class.              * Emission Factor (EF), emission factor for combustion of jet fuel.              * PreProduction accounts for the CO2 emissions through pre-production              * of jet fuel/kerosene and fuel combustion.               * Multiplier accounts for the warming effect due to non-CO2 aircraft emissions.              * a , b and c are used in ax^2 + bx + c which is a nonlinear approximation              * for fuel emissions during landing and takeoff cycle including taxi */ 
-        public Flight()
-        {
-            this.Color = SKColor.Parse("FFB0F6");
-            this.SVGIcon = "M58,73.5 C58,73.5 152.5,26.5 200,3.5 C227,-9 251.5,17 227,35.5 C219,41.5 187,56.5 187,56.5 L149,162.5 L124,175.5 L132,83.5 C132,83.5 67,113.5 45,107.5 C26.5,102.5 0,64.5 0,64.5 L9,57.5 L58,73.5 M55,15.5 L95,43.5 L136,23.5 L76,3.5 L55,15.5 Z";
-            this.KgCO2 = CalculateCO2(94);
-        }
+        /* Constants used in formula explained, for more information see              * myclimate.org              * Average seat number (S), average seat number across al cabin classes.               * Passenger Load Factor (PLF) is the average load factor on flights              * Detour Constant (DC) is async distance correction for detours and               * holding patterns, and inefficiencies in the air traffic control systems.               * Cargo Factor (CF), to allocate some of the emissions to cargo load such              * as freight and mail. Number used is in reality (1 - CF).               * Weight Class (WC), takes into account the average seat area in the cabin class.              * Emission Factor (EF), emission factor for combustion of jet fuel.              * PreProduction accounts for the CO2 emissions through pre-production              * of jet fuel/kerosene and fuel combustion.               * Multiplier accounts for the warming effect due to non-CO2 aircraft emissions.              * a , b and c are used in ax^2 + bx + c which is a nonlinear approximation              * for fuel emissions during landing and takeoff cycle including taxi */
 
         public int Id { get; set; }
         public double KgCO2 { get; set; }
@@ -19,7 +13,16 @@ namespace DAT190_Bachelor_Project.Model
         public string SVGIcon { get; set; }
 
         // Constants used for emission calculation
-        private double shortHaulTreshold = 1500;         private double longHaulTreshold = 2500;         private double s = 158.44;         private double plf = 0.77;         private double dc = 50.00;         private double cf = 0.951;         private double cw = 1.26;         private double ef = 3.15;         private double preProduction = 0.51;         private double multiplier = 2.00;
+        private double shortHaulTreshold = 1500;
+        private double longHaulTreshold = 2500;
+        private double s = 158.44;
+        private double plf = 0.77;
+        private double dc = 50.00;
+        private double cf = 0.951;
+        private double cw = 1.26;
+        private double ef = 3.15;
+        private double preProduction = 0.51;
+        private double multiplier = 2.00;
         private double a;
         private double b;
         private double c;
@@ -29,13 +32,19 @@ namespace DAT190_Bachelor_Project.Model
         // Long-haul 36.14 USD, updated 2017
         // Average cost/100km = 17.04 USD = 132.06 NOK/100 km = 1.3206 NOK/km
         // 1 USD = 7,75 NOK
-        double pricePerKm = 1.3206;
+        private double pricePerKm = 1.3206;
+ 
+        public Flight()
+        {
+            this.Color = SKColor.Parse("FFB0F6");
+            this.SVGIcon = "M58,73.5 C58,73.5 152.5,26.5 200,3.5 C227,-9 251.5,17 227,35.5 C219,41.5 187,56.5 187,56.5 L149,162.5 L124,175.5 L132,83.5 C132,83.5 67,113.5 45,107.5 C26.5,102.5 0,64.5 0,64.5 L9,57.5 L58,73.5 M55,15.5 L95,43.5 L136,23.5 L76,3.5 L55,15.5 Z";
+            this.KgCO2 = CalculateCO2(94);
+        }
 
+       
         public double CalculateCO2(double amount)
         {
-            
-
-             double km = amount / pricePerKm;              // Short og long haul flight             if(km < shortHaulTreshold) {                 a = 0.0000387871;                 b = 2.9866;                 c = 1263.42;             } else if (m > longHaulTreshold) {                 a = 0.000134576;                 b = 6.1798;                 c = 3446.20;             } else {                 a = (0.0000387871 + 0.000134576) / 2;                 b = (2.9866 + 6.1798) / 2;                 c = (1263.42 + 3446.20) / 2;             }              double Emission = ((a * Math.Pow(km + dc, 2) + b * (km + dc) + c) / (a * plf)) * cf * cw * (ef * multiplier + preProduction);             return Emission;
+                         double km = amount / pricePerKm;              // Short og long haul flight             if(km < shortHaulTreshold) {                 a = 0.0000387871;                 b = 2.9866;                 c = 1263.42;             } else if (km > longHaulTreshold) {                 a = 0.000134576;                 b = 6.1798;                 c = 3446.20;             } else {                 a = (0.0000387871 + 0.000134576) / 2;                 b = (2.9866 + 6.1798) / 2;                 c = (1263.42 + 3446.20) / 2;             }              double Emission = ((a * Math.Pow(km + dc, 2) + b * (km + dc) + c) / (s * plf)) * cf * cw * (ef * multiplier + preProduction);             return Emission;
         }
     }
 }
--- DAT190_Bachelor_Project/Model/Vehicle.cs
+++ DAT190_Bachelor_Project/Model/Vehicle.cs
@@ -3,95 +3,95 @@ namespace DAT190_Bachelor_Project.Model
 {
     public class Vehicle
     {
-        private string RegistrationNumber;
-        private VehicleSize Size;
+        private string registrationNumber;
+        private VehicleSize size;
         public FuelType FuelType { get; set; }
         public double FuelConsumptionPerKm { get; set; }
-        private double AverageCO2EmissionPerKm;
+        private double averageCO2EmissionPerKm;
 
 
-        public Vehicle(string RegistrationNumber, VehicleSize Size, FuelType FuelType, double ConsumptionPerKm )
+        public Vehicle(string registrationNumber, VehicleSize size, FuelType fuelType, double consumptionPerKm )
         {
-            this.RegistrationNumber = RegistrationNumber;
-            this.Size = Size;
-            this.FuelType = FuelType;
-            this.FuelConsumptionPerKm = ConsumptionPerKm;
+            this.registrationNumber = registrationNumber;
+            this.size = size;
+            this.FuelType = fuelType;
+            this.FuelConsumptionPerKm = consumptionPerKm;
 
             // If vehicle is motorbike, fuel type must be petrol
-            if (Size == VehicleSize.Motorbike && FuelType != FuelType.Petrol) {
+            if (size == VehicleSize.Motorbike && FuelType != FuelType.Petrol) {
                 this.FuelType = FuelType.Petrol;
             }
 
         }
 
-        public void SetAvergageCO2EmissionPerKm(FuelType Type, VehicleSize Size) {
+        public void SetAvergageCO2EmissionPerKm(FuelType type, VehicleSize size) {
 
             // All numbers used in the calculation are provided by the UK Government
             // Conversion Factors for greenhouse gas (GHG) reporting (2017).
 
            // Fuel type is Petrol
-            if (Type == FuelType.Petrol) {
-                switch (Size)
+            if (type == FuelType.Petrol) {
+                switch (size)
                 {
                     case VehicleSize.Small:
-                        AverageCO2EmissionPerKm = 0.15649;
+                        averageCO2EmissionPerKm = 0.15649;
                         break;
                     case VehicleSize.Medium:
-                        AverageCO2EmissionPerKm = 0.19407;
+                        averageCO2EmissionPerKm = 0.19407;
                         break;
                     case VehicleSize.Large:
-                        AverageCO2EmissionPerKm = 0.28539;
+                        averageCO2EmissionPerKm = 0.28539;
                         break;
                     case VehicleSize.Motorbike:
-                        AverageCO2EmissionPerKm = 0.11662;
+                        averageCO2EmissionPerKm = 0.11662;
                         break;
                     default:
                         // Default is set to average car value
-                        AverageCO2EmissionPerKm = 18568;
+                        averageCO2EmissionPerKm = 18568;
                         break;
                 }
             } 
             // Fuel type is Diesel
-            else if (Type == FuelType.Diesel) {
-                    switch (Size)
+            else if (type == FuelType.Diesel) {
+                    switch (size)
                     {
                         case VehicleSize.Small:
-                            AverageCO2EmissionPerKm = 0.14545;
+                            averageCO2EmissionPerKm = 0.14545;
                             break;
                         case VehicleSize.Medium:
-                            AverageCO2EmissionPerKm = 0.1738;
+                            averageCO2EmissionPerKm = 0.1738;
                             break;
                         case VehicleSize.Large:
-                            AverageCO2EmissionPerKm = 0.21834;
+                            averageCO2EmissionPerKm = 0.21834;
                             break;
                         default:
                             // Default is set to average car value
-                            AverageCO2EmissionPerKm = 17887;
+                            averageCO2EmissionPerKm = 17887;
                             break;
                 }
             }
             // Fuel type is Hybrid
-            else if (Type == FuelType.PlugInHybrid) {
-                switch (Size)
+            else if (type == FuelType.PlugInHybrid) {
+                switch (size)
                 {
                     case VehicleSize.Small:
-                        AverageCO2EmissionPerKm = 0.10973;
+                        averageCO2EmissionPerKm = 0.10973;
                         break;
                     case VehicleSize.Medium:
-                        AverageCO2EmissionPerKm = 0.11243;
+                        averageCO2EmissionPerKm = 0.11243;
                         break;
                     case VehicleSize.Large:
-                        AverageCO2EmissionPerKm = 0.13052;
+                        averageCO2EmissionPerKm = 0.13052;
                         break;
                     default:
                         // Default is set to average car value
-                        AverageCO2EmissionPerKm = 11659;
+                        averageCO2EmissionPerKm = 11659;
                         break;
                 }
             }
         }
         public double GetAverageCO2EmissionPerKm() {
-            return AverageCO2EmissionPerKm;
+            return averageCO2EmissionPerKm;
         }
 
     }
