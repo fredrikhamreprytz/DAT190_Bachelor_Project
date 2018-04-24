37e9d5c7de1f989bbaa978625db05bdd836d056e
oysteinlervik <oystein.lervik@outlook.com>
2018-04-24 10:39:54 +0200

    Household database added and some refactoring

--- DAT190_Bachelor_Project/Model/Household.cs
+++ DAT190_Bachelor_Project/Model/Household.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using SkiaSharp;
+using SQLite;
+
 namespace DAT190_Bachelor_Project.Model
 {
     public class Household : IEmission
     {
         // CO2-ekvivalent per kW/h in Norway from electricitymap.org
-       private double kgCO2EkvPerkWh = 0.016;
+        private double kgCO2EkvPerkWh = 0.016;
 
         // Price per kW/h from ssb.no
         // Last updated 4. quarter 2017, price in NOK
@@ -17,9 +19,11 @@ namespace DAT190_Bachelor_Project.Model
             this.SVGIcon = "M239.5,120.5 L239.5,260.5 C223,260.5 203,260.5 181.5,260.5 L181.5,180.5 L100.5,180.5 L100.5,260.5 C66,260.5 40.5,260.5 40.5,260.5 L40.5,120.5 L0.5,120.5 L140.5,0.5 L280.5,120.5 L239.5,120.5 Z";
         }
 
+        [PrimaryKey]
         public int Id { get; set; }
         public double KgCO2 { get; set; }
         public DateTime Date { get; set; }
+        [Ignore]
         public SKColor Color { get; set; }
         public string SVGIcon { get; set; }
 
--- DAT190_Bachelor_Project/Model/IEmission.cs
+++ DAT190_Bachelor_Project/Model/IEmission.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using SkiaSharp;
+using SQLite;
+
 namespace DAT190_Bachelor_Project.Model
 {
     public interface IEmission
     {
         // Properties
-        int Id {get; set;}
+        int Id { get; set; }
         double KgCO2 { get; set; }
         DateTime Date { get; set; }
-        SKColor Color { get; set;  }
+        SKColor Color { get; set; }
         string SVGIcon { get; set; }
 
         // Methods
