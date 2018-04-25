48b7223f2ff18a5174e9a833db07c544acc9e39b
h129411 <jorgenschreiner@hotmail.com>
2018-04-25 15:10:34 +0200

    Added UI and Unit test projects

---
+++ UITests/FrontPageTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITests
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class FrontPageTests
+    {
+        IApp app;
+        Platform platform;
+
+        public FrontPageTests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void CO2EmissionCakeCanvasIsShown()
+        {
+            // For the Marked() method to work crossplatformly, the xaml element must have the AutomationId="someIdString" property set.
+            AppResult[] results = app.WaitForElement(c => c.Marked("carbonFootprintCanvas"));
+            Assert.IsNotEmpty(results);
+        }
+    }
+}
---
+++ UnitTests/FlightTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+
+    public class FlightTests
+    {
+
+
+        public FlightTests()
+        {
+
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            
+        }
+
+        [Test]
+        public void CalculateCO2ReturnsZeroForZero()
+        {
+            // Eksempeltest
+            Assert.IsTrue(true);
+        }
+    }
+}
